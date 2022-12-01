f6d527aafd676c57cfe979d7640f92d9ba6eca3a
phamtanphat <phatdroid94@gmail.com>
2022-12-01 18:45:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+//1 : khai bao bien
+// let name = "Pham Tan Phat"
+// let age = 28
+// let address = "Quan 10"
+// let isMale = true
+
+// const CATEGORY = "Person"
+
+// 2: Câu điều kiên
+
+// let a = 5
+// let b = 10
+
+// if (NaN) {
+//     console.log("A lon hon B")
+// } else {
+//     console.log("A be hon hoac bang B")
+// }
+
+// let a = 5
+
+// switch(a) {
+//     case 5 : 
+//         console.log(a)
+//         break
+// }
+
+// 3: Object
+// const teo = {
+//     "name": "Nguyen Van Teo",
+//     "age" : 5,
+//     "info": function() {
+//         console.log(this.name)
+//     }
+// }
+
+// 4: Function
+
+// function print(message) {
+//     console.log(message)
+// }
+
+// print("a")
+
+// 5: Array
+let array = []
+
+// Thêm
+array.push("Teo")
+array.push("Ti")
+array.push("Tun")
+// Thêm ở đầu
+// array.unshift("Phat")
+// Thêm ở giữa
+// array.splice(1, 0, "Hoa", "Ly")
+// Xoá
+// array.pop()
+// Xoá ở giữa
+array.splice(1, 1)
+
+console.log(array)
