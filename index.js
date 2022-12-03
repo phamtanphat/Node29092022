ef3ba52f488633798750cf0254320cf0616bcefc
phamtanphat <phatdroid94@gmail.com>
2022-12-03 18:43:01 +0700

    get post

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
-//1 : khai bao bien
-// let name = "Pham Tan Phat"
-// let age = 28
-// let address = "Quan 10"
-// let isMale = true
+/*1 : khai bao bien
+let name = "Pham Tan Phat"
+let age = 28
+let address = "Quan 10"
+let isMale = true
 
-// const CATEGORY = "Person"
+const CATEGORY = "Person"
 
-// 2: Câu điều kiên
+2: Câu điều kiên
 
-// let a = 5
-// let b = 10
+let a = 5
+let b = 10
 
-// if (NaN) {
-//     console.log("A lon hon B")
-// } else {
-//     console.log("A be hon hoac bang B")
-// }
+if (NaN) {
+    console.log("A lon hon B")
+} else {
+    console.log("A be hon hoac bang B")
+}
 
-// let a = 5
+let a = 5
 
-// switch(a) {
-//     case 5 : 
-//         console.log(a)
-//         break
-// }
+switch(a) {
+    case 5 : 
+        console.log(a)
+        break
+}
 
-// 3: Object
-// const teo = {
-//     "name": "Nguyen Van Teo",
-//     "age" : 5,
-//     "info": function() {
-//         console.log(this.name)
-//     }
-// }
+3: Object
+const teo = {
+    "name": "Nguyen Van Teo",
+    "age" : 5,
+    "info": function() {
+        console.log(this.name)
+    }
+}
 
-// 4: Function
+4: Function
 
-// function print(message) {
-//     console.log(message)
-// }
+function print(message) {
+    console.log(message)
+}
 
-// print("a")
+print("a")
 
-// 5: Array
-// let array = []
+5: Array
+let array = []
 
-// Thêm
-// array.push("Teo")
-// array.push("Ti")
-// array.push("Tun")
-// Thêm ở đầu
-// array.unshift("Phat")
-// Thêm ở giữa
-// array.splice(1, 0, "Hoa", "Ly")
-// Xoá
-// array.pop()
-// Xoá ở giữa
-// array.splice(1, 1)
+Thêm
+array.push("Teo")
+array.push("Ti")
+array.push("Tun")
+Thêm ở đầu
+array.unshift("Phat")
+Thêm ở giữa
+array.splice(1, 0, "Hoa", "Ly")
+Xoá
+array.pop()
+Xoá ở giữa
+array.splice(1, 1)
 
-// console.log(array)
+console.log(array)
 
-// 6: Function Class
+6: Function Class
 
-// function Person(name, age) {
-//     this.name = name;
-//     this.age = age
-//     this.info = function () {
-//         console.log(this.name)
-//     }
-// }
+function Person(name, age) {
+    this.name = name;
+    this.age = age
+    this.info = function () {
+        console.log(this.name)
+    }
+}
 
-// let teo = new Person("Teo", 10)
-// teo.info()
+let teo = new Person("Teo", 10)
+teo.info()
 
-// 7: Bat dong bo
+7: Bat dong bo
 
 let message = ""
 
@@ -95,3 +95,22 @@ function handle() {
         }, 2000)
     })
 }
+*/
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+// route
+// params
+// app.get('/', function (req, res) {
+//     let page = req.query["page"]
+//     res.send(`Page ${page}`)
+// })
+
+app.post('/send', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(3000)
